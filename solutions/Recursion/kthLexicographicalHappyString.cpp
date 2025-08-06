38ce59dcdd50e9b5b671cddfe95aa8737b663fea
ashutoshsao <ashutoshsao17@gmail.com>
2025-08-06 09:48:02 +0530

    made changes

--- solutions/Recursion/kthLexicographicalHappyString.cpp
+++ solutions/Recursion/kthLexicographicalHappyString.cpp
@@ -1,13 +1,22 @@
 /*
 Problem: The k-th Lexicographical String of All Happy Strings of Length n
-Link: https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/
+Link:
+https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/
 Problem Statement: A happy string is a string that:
 - consists only of letters of the set ['a', 'b', 'c'].
 - s[i] != s[i+1] for all i from 0 to s.length - 2 (string is 1-indexed).
-Given two integers n and k, return the k-th lexicographical string among all happy strings of length n. If there are less than k happy strings of length n, return an empty string.
-Approach: The solution uses a recursive approach to generate all happy strings of length n. A helper function `getStrings` is used to build the strings. The function is called recursively, and at each step, it appends a character from the set {'a', 'b', 'c'} that is different from the last character of the current string. When a string of length n is formed, it is added to a result vector. Finally, the k-th string from the sorted list of happy strings is returned.
-Time Complexity: O(3 * 2^(n-1)) which is O(2^n) because for the first character we have 3 choices and for the rest n-1 characters, we have 2 choices.
-Space Complexity: O(k) where k is the number of happy strings, to store the result vector.
+Given two integers n and k, return the k-th lexicographical string among all
+happy strings of length n. If there are less than k happy strings of length n,
+return an empty string. Approach: The solution uses a recursive approach to
+generate all happy strings of length n. A helper function `getStrings` is used
+to build the strings. The function is called recursively, and at each step, it
+appends a character from the set {'a', 'b', 'c'} that is different from the last
+character of the current string. When a string of length n is formed, it is
+added to a result vector. Finally, the k-th string from the sorted list of happy
+strings is returned. Time Complexity: O(3 * 2^(n-1)) which is O(2^n) because for
+the first character we have 3 choices and for the rest n-1 characters, we have 2
+choices. Space Complexity: O(k) where k is the number of happy strings, to store
+the result vector.
 */
 #include <iostream>
 #include <string>
@@ -56,10 +65,10 @@ public:
 };
 
 int main() {
-  Solution solution;
+  Solution sol;
   int n = 3;
   int k = 9;
-  string happyString = solution.getHappyString(n, k);
+  string happyString = sol.getHappyString(n, k);
   cout << happyString << endl;
   return 0;
 }
