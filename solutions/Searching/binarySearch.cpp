58c826e6da60c4d4a8628a418d30e23e82845318
ashutoshsao <ashutoshsao17@gmail.com>
2025-08-21 23:16:02 +0530

    modified solution

--- solutions/Searching/binarySearch.cpp
+++ solutions/Searching/binarySearch.cpp
@@ -1,10 +1,17 @@
 /*
  Problem: Binary Search
  Link: https://leetcode.com/problems/binary-search/description/
- Problem Statement: Given a sorted array of integers `nums` and an integer `target`, write a function to search for the `target` within `nums`. If the `target` value exists in the array, the function should return its index. If the `target` is not found, the function must return -1. The algorithm must have a runtime complexity of O(log n).
- Approach: The solution uses an iterative binary search approach. It initializes `low` and `high` pointers to the start and end of the array. In each iteration, it calculates the middle index `m`. If the middle element is the target, it returns the index. If the middle element is less than the target, it moves the `low` pointer to `m + 1`. Otherwise, it moves the `high` pointer to `m - 1`. If the loop finishes without finding the target, it returns -1.
- Time Complexity: O(log n)
- Space Complexity: O(1)
+ Problem Statement: Given a sorted array of integers `nums` and an integer
+ `target`, write a function to search for the `target` within `nums`. If the
+ `target` value exists in the array, the function should return its index. If
+ the `target` is not found, the function must return -1. The algorithm must have
+ a runtime complexity of O(log n). Approach: The solution uses an iterative
+ binary search approach. It initializes `low` and `high` pointers to the start
+ and end of the array. In each iteration, it calculates the middle index `m`. If
+ the middle element is the target, it returns the index. If the middle element
+ is less than the target, it moves the `low` pointer to `m + 1`. Otherwise, it
+ moves the `high` pointer to `m - 1`. If the loop finishes without finding the
+ target, it returns -1. Time Complexity: O(log n) Space Complexity: O(1)
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,15 +19,15 @@ using namespace std;
 class Solution {
 public:
   int search(vector<int> &nums, int k) {
-    int low = 0, high = nums.size() - 1;
-    while (low <= high) {
-      int m = low + (high - low) / 2;
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int m = left + (right - left) / 2;
       if (nums[m] == k) {
         return m;
       } else if (nums[m] < k) {
-        low = m + 1;
+        left = m + 1;
       } else {
-        high = m - 1;
+        right = m - 1;
       }
     }
     return -1;
